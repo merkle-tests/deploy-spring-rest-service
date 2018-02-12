c666bca674fdca0a05301c29344d2a64f2358aac
Gimenez, Aleix <Aleix.Gimenez@sky.uk>
2018-02-12 13:09:46 +0100

    Move request mapping from controller to entrypoint

--- spring-rest/src/main/java/com/github/unqualsevol/spring/HelloWorldController.java
+++ spring-rest/src/main/java/com/github/unqualsevol/spring/HelloWorldController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.*;
  * Created by agimenez on 19/09/17.
  */
 @RestController
-@RequestMapping("/helloworld")
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
     @ResponseBody
     Message helloworld(@RequestParam("name") String userId) {
         return new Message(userId);
